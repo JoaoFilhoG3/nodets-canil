d6b19199f27a5f686130f01964843aa9d7c4945b
João Filho <joao.filho.g3@gmail.com>
2023-05-23 07:36:52 -0300

    Commit inicial do projeto.

---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import mustache from "mustache-express";
+import path from "path";
+import mainRoutes from "./routes/index";
+
+//Configurando dotenv
+dotenv.config();
+
+const server = express();
+
+//Configurando Mustache
+server.set("view engine", "mustache");
+server.set("views", path.join(__dirname, "views"));
+server.engine("mustache", mustache());
+
+//Configurando Pasta Pública
+server.use(express.static(path.join(__dirname, "../public")));
+
+//Configurando Rotas
+server.use(mainRoutes);
+
+//Configurando mensagem de página não encontrada
+server.use((req:Request, res:Response)=>{
+    res.status(404).send("Página não encontrada.");
+});
+
+server.listen(process.env.PORT);
